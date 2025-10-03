ba93b4bc8e63c165a555eeaefe5f5bab1d1134b0
bautw324 <bautwzz@gmail.com>
2025-10-03 17:00:59 -0300

    Cree una clase As de Espadas del punto 4

---
+++ src/AsEspadas.java
@@ -0,0 +1,11 @@
+public class AsEspadas {
+    public static void main(String[] args) {
+        while (true) {
+            NaipeEspañol naipe = RevisaTruco.generarCarta();
+            if (naipe.getNombre().equals("As de espada[1]")) {
+                System.out.println("Se encontró el " + naipe.getNombre());
+                break;
+            }
+        }
+    }
+}
