75a6ed2d69c584d3813cd07a4df76480b2211887
bautw324 <112228409+bautw324@users.noreply.github.com>
2025-10-03 23:01:54 -0300

    Hice cambios para hacer el punto 10

--- src/NaipeEspañol.java
+++ src/NaipeEspañol.java
@@ -7,36 +7,39 @@ public class NaipeEspañol extends Naipe{
     public void asignarNombre(){
         switch (getNumero()){
             case 1:
-                this.setNombre("Ancho" + " de " + getPalo() + " [1]");
+                this.setNombre("[1] Ancho de ");
                 break;
             case 2:
-                this.setNombre("Dos" + " de " + getPalo() + " [2]");
+                this.setNombre("[2] Dos de ");
                 break;
             case 3:
-                this.setNombre("Tres" + " de " + getPalo() + " [3]");
+                this.setNombre("[3] Tres de ");
                 break;
             case 4:
-                this.setNombre("Cuatro" + " de " + getPalo() + " [4]");
+                this.setNombre("[4] Cuatro de ");
                 break;
             case 5:
-                this.setNombre("Cinco" + " de " + getPalo() + " [5]");
+                this.setNombre("[5] Cinco de " );
                 break;
             case 6:
-                this.setNombre("Seis" + " de " + getPalo() + " [6]");
+                this.setNombre("[6] Seis de ");
                 break;
             case 7:
-                this.setNombre("Siete" + " de " + getPalo() + " [7]");
+                this.setNombre("[7] Siete de ");
                 break;
             case 10:
-                this.setNombre("Sota" + " de " + getPalo() + " [10]");
+                this.setNombre("[10] Sota de ");
                 break;
             case 11:
-                this.setNombre("Caballo" + " de " + getPalo() + " [11]");
+                this.setNombre("[11] Caballo de ");
                 break;
             case 12:
-                this.setNombre("Rey" + " de " + getPalo() + " [12]");
+                this.setNombre("[12] Rey de ");
                 break;
         }
+        this.setNombre(this.getNombre() + getPalo());
+
+
     }
 
 
--- src/NaipeIngles.java
+++ src/NaipeIngles.java
@@ -9,44 +9,45 @@ public class NaipeIngles extends Naipe{
                 this.setNombre("Joker");
                 break;
             case 1:
-                this.setNombre("As de " + getPalo() + " [A]");
+                this.setNombre("[A] As de ");
                 break;
             case 2:
-                this.setNombre("Dos de " + getPalo() + " [2]");
+                this.setNombre("[2] Dos de ");
                 break;
             case 3:
-                this.setNombre("Tres de " + getPalo() + " [3]");
+                this.setNombre("[3] Tres de ");
                 break;
             case 4:
-                this.setNombre("Cuatro de " + getPalo() + " [4]");
+                this.setNombre("[4] Cuatro de ");
                 break;
             case 5:
-                this.setNombre("Cinco de " + getPalo() + " [5]");
+                this.setNombre("[5] Cinco de ");
                 break;
             case 6:
-                this.setNombre("Seis de " + getPalo() + " [6]");
+                this.setNombre("[6] Seis de ");
                 break;
             case 7:
-                this.setNombre("Siete de " + getPalo() + " [7]");
+                this.setNombre("[7] Siete de ");
                 break;
             case 8:
-                this.setNombre("Ocho de " + getPalo() + " [8]");
+                this.setNombre("[8] Ocho de ");
                 break;
             case 9:
-                this.setNombre("Nueve de " + getPalo() + " [9]");
+                this.setNombre("[9] Nueve de ");
                 break;
             case 10:
-                this.setNombre("Diez de " + getPalo() + " [10]");
+                this.setNombre("[10] Diez de ");
                 break;
             case 11:
-                this.setNombre("Sota de " + getPalo() + " [J]");
+                this.setNombre("[J] Sota de ");
                 break;
             case 12:
-                this.setNombre("Reina de " + getPalo() + " [Q]");
+                this.setNombre("[Q] Reina de ");
                 break;
             case 13:
-                this.setNombre("Rey de " + getPalo() + " [K]");
+                this.setNombre("[K] Rey de ");
                 break;
         }
+        this.setNombre(getNombre() + getPalo());
     }
 }
