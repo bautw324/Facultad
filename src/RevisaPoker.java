01879f65eb1fcbfb60fc2fea74be0f39c2deda7b
bautw324 <112228409+bautw324@users.noreply.github.com>
2025-10-10 13:26:29 -0300

    Completé RevisaPoker

--- src/RevisaPoker.java
+++ src/RevisaPoker.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 public class RevisaPoker {
 
@@ -26,8 +27,51 @@ public class RevisaPoker {
         return (n.getPalo().equals("Joker"));
     }
 
+    public static boolean esEscalera3(NaipeIngles n1, NaipeIngles n2, NaipeIngles n3){
+        int[] cartas = {n1.getNumero(), n2.getNumero(), n3.getNumero()};
+        Arrays.sort(cartas);
+        return (cartas[1]==cartas[0]+1 && cartas[2] == cartas[1]+1);
+    }
+
     private static boolean esTrio(NaipeIngles n1, NaipeIngles n2, NaipeIngles n3){
         return (n1.mismoValor(n2) && n2.mismoValor(n3));
     }
 
+    public static void evaluarMano5(NaipeIngles n1, NaipeIngles n2, NaipeIngles n3, NaipeIngles n4, NaipeIngles n5){
+        int[] numeros = {n1.getNumero(), n2.getNumero(), n3.getNumero(), n4.getNumero(), n5.getNumero()};
+        String[] palos = {n1.getPalo(), n2.getPalo(), n3.getPalo(), n4.getPalo(), n5.getPalo()};
+        Arrays.sort(numeros);
+
+        boolean esEscalera = (numeros[1] == numeros[0]+1 && numeros[2] == numeros[1]+1 &&
+                numeros[3] == numeros[2]+1 && numeros[4] == numeros[3]+1);
+
+        boolean mismoPalo = palos[0].equals(palos[1]) && palos[1].equals(palos[2]) &&
+                palos[2].equals(palos[3]) && palos[3].equals(palos[4]);
+
+        if (esEscalera) {
+            if (mismoPalo) {
+                System.out.println("Las cartas son una escalera real");
+            } else {
+                System.out.println("Las cartas son una escalera");
+            }
+            return;
+        }
+
+        if ((numeros[0] == numeros[1] && numeros[1] == numeros[2] && numeros[2] == numeros[3]) ||
+                (numeros[1] == numeros[2] && numeros[2] == numeros[3] && numeros[3] == numeros[4])) {
+            System.out.println("Las cartas conforman un póker");
+            return;
+        }
+
+        boolean esFull =
+                (numeros[0] == numeros[1] && numeros[1] == numeros[2] && numeros[3] == numeros[4] && numeros[2] != numeros[3]) ||  // AAABB
+                (numeros[0] == numeros[1] && numeros[2] == numeros[3] && numeros[3] == numeros[4] && numeros[1] != numeros[2]);  // AABBB
+
+        if (esFull) {
+            System.out.println("Las cartas conforman un full");
+            return;
+        }
+
+        System.out.println("No es ninguna combinación especial");
+    }
 }
